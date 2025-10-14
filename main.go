88e60f9f857123267a7c68331ee00616d9f1f53f
delroscol98 <collindelrosario98@gmail.com>
2025-10-15 09:42:45 +1100

    tests for cleanInput function

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	trimmedText := strings.ToLower(strings.TrimSpace(text))
+
+	if len(trimmedText) == 0 {
+		return []string{}
+	}
+
+	words := strings.Split(trimmedText, " ")
+	return words
 }
---
+++ repl_test.go
@@ -0,0 +1,38 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		{
+			input:    "       ",
+			expected: []string{},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf(`slice lengths do not match: expected: %v, actual: %v`, len(c.expected), len(actual))
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf(`words in slice do not match: expected: %v, actual: %v`, expectedWord, word)
+			}
+		}
+	}
+}
